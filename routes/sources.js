ae72b5deb73317a4e5faf11b2752f11bb357c55d
erick-commits <erickfernandomoraramirez@gmail.com>
2020-10-14 19:15:10 -0400

    new database

---
+++ routes/sources.js
@@ -0,0 +1,40 @@
+const DataAccess = require("../data/sqliteDataAccess");
+let express = require("express");
+let router = express.Router();
+
+router.get("/:source?", function (req, res) {
+  let dataAccess = new DataAccess("./data/news.db");
+  let parameters = [];
+  let query = "select * from vw_sources;";
+  if (req.params.category) {
+    parameters = [req.params.category];
+    query = "select * from vw_sources where source = ?;";
+  }
+  dataAccess.select(query, parameters, (err, data) => {
+    if (err) {
+      res.status(500).json("Algo se incendia...");
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+router.get("/getOrdered/:top?", function (req, res) {
+  let dataAccess = new DataAccess("./data/news.db");
+  let parameters = [];
+  let query = "select * from vw_news_with_order";
+  if (req.params.top) {
+    parameters = [req.params.top];
+    query = " where ord <= ?;";
+  }
+  query += ";";
+  dataAccess.select(query, parameters, (err, data) => {
+    if (err) {
+      res.status(500).json("Algo se incendia...");
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+module.exports = router;
